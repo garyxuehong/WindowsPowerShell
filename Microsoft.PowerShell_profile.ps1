c276a041c5701c25bd05ff168ce58157c62658e5
Gary Xue <gary.xuehong@gmail.com>
2021-03-19 00:26:33 +1100

    add and fix fzf

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,25 @@
-function Get-GitStatus { & git lg -sb $args }
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme paradox
+
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+Remove-PSReadlineKeyHandler 'Ctrl+t'
+Import-Module PSFzf
+
+Import-Module ZLocation
+
+Set-Alias l ls
+Set-Alias ll ls
+Set-Alias la ls
+
+function Get-GitStatus { & git status ; git lg }
 New-Alias -Name lg -Value Get-GitStatus -Force -Option AllScope
+
+function Add-GitFiles { & git add . ; git commit }
+New-Alias -Name gac -Value Add-GitFiles -Force -Option AllScope
+
+function List-ZF { & z | cut -b 8- | fzf }
+function Go-ZF { 
+  cd (List-ZF).Trim();
+}
+New-Alias -Name zf -Value Go-ZF -Force -Option AllScope
